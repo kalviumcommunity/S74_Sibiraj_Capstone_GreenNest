98a04ca6ddf63db06480bfd227f9a7f93f446ed2
Sibi Shalom <shalomsibi16@gmail.com>
2026-02-04 12:38:30 +0530

    update-viteconfig

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.BACKEND_URL || 'http://localhost:5002',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  }
 })
